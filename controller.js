b58d700e4a65b071f272149de37f0e1e3e15d1f3
vildesv <vilde@getacademy.no>
2021-04-20 21:03:49 +0200

    yas

--- controller.js
+++ controller.js
@@ -1,7 +1,61 @@
 // Controller
+function svar(denne, nummer) {
 
-function svar(denvitryktepå) {
+    if(antall < 5){
 
-    svartext = `Du svarte: ${denvitryktepå}`;
+        if(nummer == 1 && svar1 == true){svartext = "Du har allerede svart på dette spørsmålet."}
+        else if(nummer == 2 && svar2 == true){svartext = "Du har allerede svart på dette spørsmålet."}
+        else if(nummer == 3 && svar3 == true){svartext = "Du har allerede svart på dette spørsmålet."}
+        else if(nummer == 4 && svar4 == true){svartext = "Du har allerede svart på dette spørsmålet."}
+        else if(nummer == 5 && svar5 == true){svartext = "Du har allerede svart på dette spørsmålet."}
+        
+        if(nummer == 1 && svar1 == false){
+            svar1 = true;
+            antallLeggTil();
+            svartext = `Du svarte ${denne} på spørsmål ${nummer}`;
+        }
+        if(nummer == 2 && svar2 == false){
+            svar2 = true;
+            antallLeggTil();
+            svartext = `Du svarte ${denne} på spørsmål ${nummer}`;
+        }
+        if(nummer == 3 && svar3 == false){
+            svar3 = true;
+            antallLeggTil();
+            svartext = `Du svarte ${denne} på spørsmål ${nummer}`;
+        }
+        if(nummer == 4 && svar4 == false){
+            svar4 = true;
+            antallLeggTil();
+            svartext = `Du svarte ${denne} på spørsmål ${nummer}`;
+        }
+        if(nummer == 5 && svar5 == false){
+            svar5 = true;
+            antallLeggTil();
+            svartext = `Du svarte ${denne} på spørsmål ${nummer}`;
+        }
+        
+        
+    }
+    else if(antall == 5) {ferdigText = "Quizen er over";}
+    else{
+        svartext = "Quizen er over";
+    }
+
     updateView()
+}
+
+function reset() {
+
+    antall = 0;
+    svar1 = false;
+    svar2 = false;
+    svar3 = false;
+    svar4 = false;
+    svar5 = false;
+
+}
+
+function antallLeggTil(){
+    return antall++;
 }
--- oblig1.js
+++ oblig1.js
@@ -2,9 +2,13 @@ var divApp = document.getElementById('app');
 
 // Model
 var svartext = '';
-
-
-
+var antall = 0;
+var ferdigText = '';
+var svar1 = false;
+var svar2 = false;
+var svar3 = false;
+var svar4 = false;
+var svar5 = false;
 
 
 
@@ -17,42 +21,44 @@ function updateView() {
     <h1>Norges Quiz</h1>
     <div id="sp1">
         <h2>Spørsmål 1: Hva heter hovedstaden i Norge?</h2>
-            <p onclick="svar('Feil')">A) Trondheim</p>
-            <p onclick="svar('Riktig!')">B) Oslo</p>
-            <p onclick="svar('Feil')">C) Bergen</p>
+            <p onclick="svar('feil', 1)">A) Trondheim</p>
+            <p onclick="svar('riktig!', 1)">B) Oslo</p>
+            <p onclick="svar('feil', 1)">C) Bergen</p>
     </div>
 
     <div id="sp2">
         <h2>Spørsmål 2: Hva er navnet på Norges høyeste fjell?</h2>
-            <p onclick="svar('Riktig!')">A) Galdhøpiggen</p>
-            <p onclick="svar('Feil')">B) Gaustatoppen</p>
-            <p onclick="svar('Feil')">C) Glittertind</p>
+            <p onclick="svar('riktig!', 2)">A) Galdhøpiggen</p>
+            <p onclick="svar('feil', 2)">B) Gaustatoppen</p>
+            <p onclick="svar('feil', 2)">C) Glittertind</p>
     </div>
 
     <div id="sp3">
         <h2>Spørsmål 3: Hva er Norges nasjonalrett?</h2>
-            <p onclick="svar('Feil')">A) Grandiosa</p>
-            <p onclick="svar('Feil')">B) Smalahove</p>
-            <p onclick="svar('Riktig!')">C) Fårikål</p>
+            <p onclick="svar('feil', 3)">A) Grandiosa</p>
+            <p onclick="svar('feil', 3)">B) Smalahove</p>
+            <p onclick="svar('riktig!', 3)">C) Fårikål</p>
     </div>
     
     <div id="sp4">
         <h2>Spørsmål 4: Når og hvor ble Grunnloven undertegnet?</h2>
-            <p onclick="svar('Feil')">A) 1914, Eidskog</p>
-            <p onclick="svar('Riktig!')">B) 1814, Eidsvoll</p>
-            <p onclick="svar('Feil')">C) 1844, Eidsfoss</p>
-            <p onclick="svar('Feil')">D) 1814, Oslo</p>
+            <p onclick="svar('feil', 4)">A) 1914, Eidskog</p>
+            <p onclick="svar('riktig!', 4)">B) 1814, Eidsvoll</p>
+            <p onclick="svar('feil', 4)">C) 1844, Eidsfoss</p>
+            <p onclick="svar('feil', 4)">D) 1814, Oslo</p>
     </div>
 
     <div id="sp5">
         <h2>Spørsmål 5: Hvem er egentlig Kongen av Gulset?</h2>
-            <p onclick="svar('Feil')">A) Kong Harald</p>
-            <p onclick="svar('Feil')">B) Jonis Josef</p>
-            <p onclick="svar('Feil')">C) Kåre Kniv</p>
-            <p onclick="svar('Riktig!')">D) Vilde Svenkesen</p>
+            <p onclick="svar('feil', 5)">A) Kong Harald</p>
+            <p onclick="svar('feil', 5)">B) Jonis Josef</p>
+            <p onclick="svar('feil', 5)">C) Kåre Kniv</p>
+            <p onclick="svar('riktig!', 5)">D) Vilde Svenkesen</p>
     </div>
 
     <div id="svarTekst">${svartext}</div>
+    <div>${ferdigText}</div>
+    <button onclick="reset()"></button>
 
     `;
 
@@ -63,12 +69,13 @@ divApp.innerHTML = html;
 // Controller ligger i egen fil...
 
 
+
 /* Oppgaven skal inneholde: 
-Skal være i Mvc (model, view, controller)
-Minst 1 Eventhandler (onclick etc.)
+Skal være i Mvc (model, view, controller) [x]
+Minst 1 Eventhandler (onclick etc.) [x]
 Minst 1 If-setning  (med && eller || , eller begge)
 Minst en funksjon som returnerer en verdi
-Minst en funksjon som bruker parameter 
+Minst en funksjon som bruker parameter [x]
 (ps. de to siste KAN være samme funksjon)
 Minst 3 unit tester til sammen av funksjonene du bruker (ikke test mot view)
 Send GitHub repolink og .io linken til både oblig og testene på mail til oblig@getacademy.no */
